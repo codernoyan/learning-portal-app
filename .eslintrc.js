725e9ad725043eec542b38641055405b2a59e0af
codernoyan <coder.noyan@gmail.com>
2023-03-29 20:39:05 +0600

    initialized

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'react/react-in-jsx-scope': 0,
+    'no-param-reassign': 0,
+    'import/prefer-default-export': 0,
+    'no-unused-vars': 0,
+    'import/no-unresolved': 0,
+    'linebreak-style': 0,
+  },
+};
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2>hello redux app</h2>
+    </div>
+  );
+}
+
+export default App;
