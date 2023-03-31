057ca4757e2e69430dd8f1a527e73ed10f5aea94
codernoyan <coder.noyan@gmail.com>
2023-03-31 16:00:19 +0600

    added seperate video player

--- src/components/student/video/Video.jsx
+++ src/components/student/video/Video.jsx
@@ -1,7 +1,9 @@
+import VideoPlayer from './VideoPlayer';
+
 export default function Video() {
   return (
     <div className="col-span-full w-full space-y-8 lg:col-span-2">
-      <iframe width="100%" className="aspect-video" src="https://www.youtube.com/embed/56zUkaXJnUA" title="Things I wish I knew as a Junior Web Developer - Sumit Saha - BASIS SoftExpo 2023" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+      <VideoPlayer />
       <div>
         <h1 className="text-lg font-semibold tracking-tight text-slate-100">
           Things I wish I knew as a Junior Web Developer - Sumit Saha - BASIS SoftExpo 2023
