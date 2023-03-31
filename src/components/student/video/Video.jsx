64292605600b7e88fcd6428a1e9e388ade677fc8
codernoyan <coder.noyan@gmail.com>
2023-03-31 20:52:34 +0600

    added dynamic video on click

--- src/components/student/video/Video.jsx
+++ src/components/student/video/Video.jsx
@@ -1,11 +1,35 @@
+import { useGetVideoQuery } from 'features/videos/videosApi';
+import { useParams } from 'react-router-dom';
+import Error from 'ui/Error';
+import Loading from 'ui/Loading';
 import VideoDescription from './VideoDescription';
 import VideoPlayer from './VideoPlayer';
 
 export default function Video() {
+  const { videoId } = useParams();
+  const {
+    data: video, isLoading, isError, error,
+  } = useGetVideoQuery(videoId);
+
+  let content = null;
+  if (isLoading) {
+    content = <Loading />;
+  } else if (!isLoading && isError) {
+    content = <Error message={error?.error} />;
+  } else if (!isLoading && !isError && Object.keys(video)?.length === 0) {
+    content = <Error message="No videos found!" />;
+  } else if (!isLoading && !isError && Object.keys(video)?.length > 0) {
+    content = (
+      <>
+        <VideoPlayer link={video.url} title={video.title} />
+        <VideoDescription video={video} />
+      </>
+    );
+  }
+
   return (
     <div className="col-span-full w-full space-y-8 lg:col-span-2">
-      <VideoPlayer />
-      <VideoDescription />
+      {content}
     </div>
   );
 }
--- src/components/student/video/VideoPlayer.jsx
+++ src/components/student/video/VideoPlayer.jsx
@@ -1,5 +1,5 @@
-export default function VideoPlayer() {
+export default function VideoPlayer({ link, title }) {
   return (
-    <iframe width="100%" className="aspect-video" src="https://www.youtube.com/embed/56zUkaXJnUA" title="Things I wish I knew as a Junior Web Developer - Sumit Saha - BASIS SoftExpo 2023" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+    <iframe width="100%" className="aspect-video" src={link} title={title} frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
   );
 }
