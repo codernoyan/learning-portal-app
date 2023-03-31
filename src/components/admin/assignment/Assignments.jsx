c2ac89ad5c4fe68395bd560bb554c88c5971c926
codernoyan <coder.noyan@gmail.com>
2023-03-31 16:41:48 +0600

    assignment page separated into 2 components

---
+++ src/components/admin/assignment/Assignments.jsx
@@ -0,0 +1,21 @@
+import Assignment from './Assignment';
+
+export default function Assignments() {
+  return (
+    <div className="overflow-x-auto mt-4">
+      <table className="divide-y-1 text-base divide-gray-600 w-full">
+        <thead>
+          <tr>
+            <th className="table-th">Title</th>
+            <th className="table-th">Video Title</th>
+            <th className="table-th">Mark</th>
+            <th className="table-th">Action</th>
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-slate-600/50">
+          <Assignment />
+        </tbody>
+      </table>
+    </div>
+  );
+}
