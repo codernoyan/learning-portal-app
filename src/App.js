bf7d36577474c0ecc0df7e40d366bd66cefd4430
codernoyan <coder.noyan@gmail.com>
2023-03-31 23:14:34 +0600

    added hook to persist authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import useAuthCheck from 'hooks/useAuthCheck';
 import { RouterProvider } from 'react-router-dom';
 import { router } from 'routes/router/router';
 import './App.css';
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  );
+  const authenticationCheck = useAuthCheck();
+  return !authenticationCheck
+    ? <div><h2>Checking Authentication</h2></div>
+    : <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/hooks/useAuthCheck.js
@@ -0,0 +1,24 @@
+import { userLogin } from 'features/auth/authSlice';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+export default function useAuthCheck() {
+  const [authChecked, setAuthChecked] = useState(false);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const localAuth = localStorage?.getItem('auth');
+
+    if (localAuth) {
+      const auth = JSON.parse(localAuth);
+      if (auth?.accessToken && auth?.user) {
+        dispatch(userLogin({
+          accessToken: auth.accessToken,
+          user: auth.user,
+        }));
+      }
+    }
+    setAuthChecked(true);
+  }, [dispatch, setAuthChecked]);
+  return authChecked;
+}
