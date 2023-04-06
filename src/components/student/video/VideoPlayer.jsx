a8f5947ef5dbdae729c6e1cc260c1d4c41f453bf
codernoyan <coder.noyan@gmail.com>
2023-04-06 13:15:12 +0600

    added autoplay to videos

--- src/components/student/video/VideoPlayer.jsx
+++ src/components/student/video/VideoPlayer.jsx
@@ -1,5 +1,5 @@
 export default function VideoPlayer({ link, title }) {
   return (
-    <iframe width="100%" className="aspect-video" src={link} title={title} frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+    <iframe width="100%" className="aspect-video" src={`${link}?autoplay=1`} title={title} frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
   );
 }
