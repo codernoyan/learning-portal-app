3697333e3f846e8f1967d28c4f11fc7d7a370a10
codernoyan <coder.noyan@gmail.com>
2023-04-06 12:09:03 +0600

    reduced leaderboard code with util function

--- src/components/student/leaderboard/RankedStudents.jsx
+++ src/components/student/leaderboard/RankedStudents.jsx
@@ -1,69 +1,70 @@
 /* eslint-disable no-plusplus */
 import { useGetAssigmentMarksQuery } from 'features/assignmentMarks/assignmentMarksApi';
-import { selectAuth } from 'features/auth/authSelector';
 import { useGetQuizMarksQuery } from 'features/quizMark/quizMarkApi';
 import { useGetUsersQuery } from 'features/users/usersApi';
-import { useSelector } from 'react-redux';
+import { indexedData, rankedData } from 'utils/leaderboardRankData';
 import RankedStudent from './RankedStudent';
 
 export default function RankedStudents() {
-  const { user: { id } } = useSelector(selectAuth);
   const {
     data: users, isLoading, isError, error,
   } = useGetUsersQuery();
   const { data: assignmentMarks } = useGetAssigmentMarksQuery();
   const { data: quizMarks, isLoading: quizLoading } = useGetQuizMarksQuery();
 
-  // working code
-  const newModifedArray = users?.map((user) => {
-    const newAssignmentMarks = assignmentMarks?.filter((assignment) => assignment?.student_id === user?.id);
-    const newQuizMarks = quizMarks?.filter((quiz) => quiz?.student_id === user?.id);
-    // reduce marks
-    const assignmentMark = newAssignmentMarks?.reduce((prev, curr) => prev + curr.mark, 0);
-    const quizMark = newQuizMarks?.reduce((prev, curr) => prev + curr.mark, 0);
-    // return a new array
-    return {
-      id: user?.id,
-      name: user?.name,
-      totalAssignmentMark: assignmentMark || 0,
-      totalQuizMark: quizMark || 0,
-    };
-  });
-  // console.log(newModifedArray);
+  // // working code
+  // const newModifedArray = users?.map((user) => {
+  //   const newAssignmentMarks = assignmentMarks?.filter((assignment) => assignment?.student_id === user?.id);
+  //   const newQuizMarks = quizMarks?.filter((quiz) => quiz?.student_id === user?.id);
+  //   // reduce marks
+  //   const assignmentMark = newAssignmentMarks?.reduce((prev, curr) => prev + curr.mark, 0);
+  //   const quizMark = newQuizMarks?.reduce((prev, curr) => prev + curr.mark, 0);
+  //   // return a new array
+  //   return {
+  //     id: user?.id,
+  //     name: user?.name,
+  //     totalAssignmentMark: assignmentMark || 0,
+  //     totalQuizMark: quizMark || 0,
+  //   };
+  // });
 
-  // provide same index number when mark is matched with others
-  // let newIndex = 0;
-  const gropuedArray = newModifedArray?.reduce((acc, current) => {
-    const sum = current.totalQuizMark + current.totalAssignmentMark;
-    if (!acc[sum]) {
-      acc[sum] = [];
-    }
-    acc[sum].push(current);
-    return acc;
-  }, {});
+  // // provide same index number when mark is matched with others
+  // // let newIndex = 0;
+  // const gropuedArray = newModifedArray?.reduce((acc, current) => {
+  //   const sum = current.totalQuizMark + current.totalAssignmentMark;
+  //   if (!acc[sum]) {
+  //     acc[sum] = [];
+  //   }
+  //   acc[sum].push(current);
+  //   return acc;
+  // }, {});
 
-  let info = null;
+  // new module system code tesing
+  const gropuedArrayNew = rankedData(users, assignmentMarks, quizMarks);
+  // testing code end
+
   let content = null;
   if (isLoading) {
     content = <tr><td>Loading....</td></tr>;
   } else if (!isLoading && isError) {
     content = <tr><td>{error?.error}</td></tr>;
-  } else if (!isLoading && !isError && newModifedArray?.length === 0) {
+  } else if (!isLoading && !isError && Object.keys(gropuedArrayNew)?.length === 0) {
     content = <tr><td>No student found!</td></tr>;
-  } else if (!isLoading && !isError && newModifedArray?.length > 0) {
+  } else if (!isLoading && !isError && Object.keys(gropuedArrayNew)?.length > 0) {
     // create a new array with new index
-    let newIndex = 0;
-    const newIndexedArray = Object.values(gropuedArray).reverse().map((group) => {
-      const finalArray = group.map((obj) => {
-        obj.index = newIndex;
-        obj.totalMark = obj.totalQuizMark + obj.totalAssignmentMark;
-        return obj;
-      });
-      newIndex++;
-      return finalArray;
-    }).flat();
-    content = newIndexedArray.slice(0, 20).map((user) => <RankedStudent key={user.id} user={user} />);
-    info = newIndexedArray?.find((loggedInUser) => loggedInUser?.id === id);
+    // let newIndex = 0;
+    // const newIndexedArray = Object.values(gropuedArray).reverse().map((group) => {
+    //   const finalArray = group.map((obj) => {
+    //     obj.index = newIndex;
+    //     obj.totalMark = obj.totalQuizMark + obj.totalAssignmentMark;
+    //     return obj;
+    //   });
+    //   newIndex++;
+    //   return finalArray;
+    // }).flat();
+    const indexedDataNew = indexedData(gropuedArrayNew);
+    content = indexedDataNew.slice(0, 20).map((user) => <RankedStudent key={user.id} user={user} />);
+    // console.log(Object.keys(gropuedArrayNew)?.length);
   }
 
   return (
---
+++ src/utils/leaderboardRankData.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-plusplus */
+export const rankedData = (users, assignmentMarks, quizMarks) => {
+  // maping over existed data and create a new array
+  const newModifedArray = users?.map((user) => {
+    const newAssignmentMarks = assignmentMarks?.filter((assignment) => assignment?.student_id === user?.id);
+    const newQuizMarks = quizMarks?.filter((quiz) => quiz?.student_id === user?.id);
+    // reduce marks
+    const assignmentMark = newAssignmentMarks?.reduce((prev, curr) => prev + curr.mark, 0);
+    const quizMark = newQuizMarks?.reduce((prev, curr) => prev + curr.mark, 0);
+    // return a new array
+    return {
+      id: user?.id,
+      name: user?.name,
+      totalAssignmentMark: assignmentMark || 0,
+      totalQuizMark: quizMark || 0,
+    };
+  });
+  // create a new gropued array for provide same index for same mark
+  const gropuedArray = newModifedArray?.reduce((acc, current) => {
+    const sum = current.totalQuizMark + current.totalAssignmentMark;
+    if (!acc[sum]) {
+      acc[sum] = [];
+    }
+    acc[sum].push(current);
+    return acc;
+  }, {});
+  return gropuedArray;
+};
+
+export const indexedData = (gropuedArray) => {
+  // create a new array with new index
+  let newIndex = 0;
+  const newIndexedArray = Object?.values(gropuedArray)?.reverse()?.map((group) => {
+    const finalArray = group?.map((obj) => {
+      obj.index = newIndex;
+      obj.totalMark = obj.totalQuizMark + obj.totalAssignmentMark;
+      return obj;
+    });
+    // icrease index number if new mark found
+    newIndex++;
+    return finalArray;
+  }).flat();
+  return newIndexedArray;
+};
