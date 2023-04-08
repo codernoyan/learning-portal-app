58a2a045570a6d83188b4116b5d0d644e29145c6
codernoyan <coder.noyan@gmail.com>
2023-04-08 18:26:11 +0600

    quiz button bugs fixed

--- src/components/student/video/VideoDescription.jsx
+++ src/components/student/video/VideoDescription.jsx
@@ -1,6 +1,9 @@
 import { useGetAssignmentByVideoIdQuery } from 'features/assignments/assignmentsApi';
+import { selectAuth } from 'features/auth/authSelector';
+import { useGetQuizMarksQuery } from 'features/quizMark/quizMarkApi';
 import { useGetQuizzesByVideoIdQuery } from 'features/quizzes/quizzesApi';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import AssignmentModal from './AssignmentModal';
 
@@ -9,14 +12,16 @@ export default function VideoDescription({ video = {} }) {
   const {
     id, title, description, createdAt,
   } = video;
-
+  const { user } = useSelector(selectAuth);
   const {
     data: assignment, isLoading, isError, error,
   } = useGetAssignmentByVideoIdQuery(id);
 
   const { data: quizzes } = useGetQuizzesByVideoIdQuery(id);
+  const { data: quizMark } = useGetQuizMarksQuery();
 
   let content = null;
+  let isAuizSubmitted = null;
   if (isLoading) {
     content = 'Loading';
   } else if (!isLoading && isError) {
@@ -25,7 +30,9 @@ export default function VideoDescription({ video = {} }) {
     content = null;
   } else if (!isLoading && !isError && Object.keys(assignment)?.length > 0) {
     content = <AssignmentModal id={id} />;
+    isAuizSubmitted = quizMark?.filter((mark) => mark.video_id === id && mark.student_id === user?.id);
   }
+  console.log(isAuizSubmitted);
 
   return (
     <div>
@@ -41,7 +48,10 @@ export default function VideoDescription({ video = {} }) {
       <div className="flex gap-4">
         {content}
         {
-          quizzes?.length !== 0 && (
+          isAuizSubmitted?.length > 0 && <h2 className="px-3 font-semibold py-1 border border-teal-500 text-white bg-teal-600 rounded-full text-sm">Quiz Sumitted</h2>
+        }
+        {
+          quizzes?.length !== 0 && isAuizSubmitted?.length === 0 && (
           <Link to={`/quiz/${id}`} className="px-3 font-bold py-1 border border-cyan text-cyan rounded-full text-sm hover:bg-cyan hover:text-primary">
             কুইজে
             অংশগ্রহণ
