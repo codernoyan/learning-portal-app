aabb276c302afcb94c5326b03c7fb124a86ab887
codernoyan <coder.noyan@gmail.com>
2023-04-08 18:42:31 +0600

    assingment completed

--- src/routes/publicRoutes/PublicStudent.jsx
+++ src/routes/publicRoutes/PublicStudent.jsx
@@ -1,8 +1,19 @@
+import { selectAuth } from 'features/auth/authSelector';
 import useAuth from 'hooks/useAuth';
-import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Navigate, useMatch } from 'react-router-dom';
 
 export default function PublicStudent({ children }) {
   const isUserLoggedIn = useAuth();
+  const { user } = useSelector(selectAuth);
+  const match = useMatch('/');
+  if (match && user?.role === 'admin') {
+    return <Navigate to="/admin" />;
+  }
 
-  return !isUserLoggedIn ? children : <Navigate to="course/videos/1" />;
+  if (!isUserLoggedIn) {
+    return children;
+  }
+  return <Navigate to="course/videos/1" />;
+  // return !isUserLoggedIn ? children : <Navigate to="course/videos/1" />;
 }
